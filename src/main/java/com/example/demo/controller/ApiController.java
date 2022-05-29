89a93a74bc1eae30f79ad4108886abe4b618bad0
jasper0831 <lyinjue456852@gmail.com>
2022-05-30 04:33:43 +0800

    第2.1版本 修正輸入使用者資訊為空時的bug、使用者資訊的json格式資料解析顯示在前端

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -4,17 +4,12 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
-import com.example.demo.model.UserInfo;
 import com.example.demo.model.message.AlertMessage;
 import com.example.demo.service.TradingService;
 import com.example.demo.utils.LogUtils;
--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import org.apache.logging.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +21,7 @@ public class MainController {
 	TradingService tradingService;
 	
 	Logger bussinessLog=LogUtils.getBussinessLogger();
+	Logger exceptionLog=LogUtils.getExceptionLogger();
 
 	@GetMapping("/main")
 	public String main(Model model) {
@@ -28,17 +31,106 @@ public class MainController {
 	@PostMapping(value="/main")
 	public String getBalance(UserInfo userInfo,Model model){
 		
-		if(userInfo==null) {
+		if(userInfo.getApiKey().equals("") || userInfo.getSecretKey().equals("")) {
 			bussinessLog.info("user info: bad user info request");
-			model.addAttribute("balanceInfo", "error UserInfo Message");
+			model.addAttribute("balanceInfo", "Please Input Complete User Information");
+		}
+		else {
+			//取得用戶餘額資訊
+			String result= tradingService.balance(userInfo);
+			model.addAttribute("balanceInfo", resultFormat(result).toString());
 		}
 		
-		//取得用戶餘額資訊
-		String result= tradingService.balance(userInfo);
-		
-		model.addAttribute("balanceInfo", result);
-		
 		return "main";
+	}
+	
+	public StringBuilder resultFormat(String result) {
+		
+		StringBuilder result2 = new StringBuilder();
+		
+		try {
+		     JSONObject jsonObject = new JSONObject(result);
+		     jsonObject.keySet().forEach(keyStr ->
+		     {
+		    	 if(keyStr.equals("data")) {
+		    		JSONObject data = (JSONObject) jsonObject.get(keyStr);
+		    		
+		    		if(!data.isEmpty()) {
+			    		for (String key : data.keySet()) {
+			    			if(key.equals("account")) {
+			    				JSONObject account = (JSONObject) data.get(key);
+			    					for (String k : account.keySet()) {
+						    			Object keyvalue = account.get(k);
+						    			switch (k) {
+										case "userId": {
+											result2.append("用戶ID: "+keyvalue.toString());
+											result2.append(System.getProperty("line.separator"));
+											break;
+										}
+										case "currency": {
+											result2.append("用戶資產: "+keyvalue.toString());
+											result2.append(System.getProperty("line.separator"));
+											break;
+										}
+										case "balance": {
+											result2.append("資產餘額: "+keyvalue.toString());
+											result2.append(System.getProperty("line.separator"));
+											break;
+										}
+										case "equity": {
+											result2.append("資產凈值: "+keyvalue.toString());
+											result2.append(System.getProperty("line.separator"));
+											break;
+										}
+										case "unrealisedPNL": {
+											result2.append("未實現盈虧: "+keyvalue.toString());
+											result2.append(System.getProperty("line.separator"));
+											break;
+										}
+										case "realisedPNL": {
+											result2.append("已實現盈虧: "+keyvalue.toString());
+											result2.append(System.getProperty("line.separator"));
+											break;
+										}
+										case "availableMargin": {
+											result2.append("可用保證金: "+keyvalue.toString());
+											result2.append(System.getProperty("line.separator"));
+											break;
+										}
+										case "usedMargin": {
+											result2.append("已用保證金: "+keyvalue.toString());
+											result2.append(System.getProperty("line.separator"));
+											break;
+										}
+										case "freezedMargin": {
+											result2.append("凍結保證金: "+keyvalue.toString());
+											result2.append(System.getProperty("line.separator"));
+											break;
+										}
+										case "longLeverage": {
+											result2.append("做多槓桿: "+keyvalue.toString());
+											result2.append(System.getProperty("line.separator"));
+											break;
+										}
+										case "shortLeverage": {
+											result2.append("做空槓桿: "+keyvalue.toString());
+											result2.append(System.getProperty("line.separator"));
+											break;
+										}
+										default:
+											break;
+										}
+			    					}
+			    			}
+			    		}
+		    		}
+		    	 }
+		     });
+		}catch (JSONException err){
+			exceptionLog.info("string parse to json exception: {}",err);
+		}
+		
+		return result2;
 	}
 	
 }
